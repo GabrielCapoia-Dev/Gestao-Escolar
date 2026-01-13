19473be411a298706dc66866718d1e784a6492e8
GabrielCapoia-Dev <gabrielcapoia11@gmail.com>
2026-01-13 11:28:54 -0300

    Adiciona modelo Escola com propriedades e relacionamentos

--- app/Models/Escola.php
+++ app/Models/Escola.php
@@ -2,9 +2,38 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 class Escola extends Model
 {
-    //
+    use HasFactory;
+    use Notifiable;
+    use HasRoles;
+
+    protected $table = 'escolas';
+
+    protected $fillable = [
+        'codigo',
+        'nome',
+        'telefone',
+        'email',
+    ];
+
+    public function casts(): array
+    {
+        return [
+            'codigo' => 'string',
+            'nome' => 'string',
+            'telefone' => 'string',
+            'email' => 'string',
+        ];
+    }
+
+    public function usuarios()
+    {
+        return $this->hasMany(User::class, 'id_escola');
+    }
 }
