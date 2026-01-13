7ea0044866e4ce4af1ec369147dd5f40fb997dde
GabrielCapoia-Dev <gabrielcapoia11@gmail.com>
2026-01-13 11:45:13 -0300

    Adiciona modelo Turma e controlador com métodos básicos para gerenciamento

---
+++ app/Models/Turma.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
+
+class Turma extends Model
+{
+    use HasFactory;
+    use Notifiable;
+
+    protected $table = 'turmas';
+
+    protected $fillable = [
+        'codigo',
+        'nome',
+        'id_serie',
+        'id_escola',
+    ];
+
+    public function casts(): array
+    {
+        return [
+            'codigo' => 'string',
+            'nome' => 'string',
+            'id_serie' => 'integer',
+            'id_escola' => 'integer',
+        ];
+    }
+
+    public function serie()
+    {
+        return $this->belongsTo(Serie::class, 'id_serie');
+    }
+
+    public function escola()
+    {
+        return $this->belongsTo(Escola::class, 'id_escola');
+    }
+}
